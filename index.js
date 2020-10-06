48997cb660a395e061db5b4f11797c9c7792b5f7
yash07sharma <yash2307sharma@gmail.com>
2020-10-06 14:40:43 +0530

    Add GET routes for login and register

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
-app.get("/", indexRouter);
+app.use("/", indexRouter);
 
 app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
